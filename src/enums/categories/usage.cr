1c1ab4cd0fb651991c36c8aeb5bb0de5236c7c98
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-06-05 01:44:30 +0200

    Remove the enum and the interface.

--- src/enums/categories/usage.cr
+++
@@ -1,7 +0,0 @@
-# Consists of identifying the category usage. For example, if a category is used for blogs,
-# it will contain a collection of blogs. This applies also for reports and projects.
-enum App::Enums::Categories::Usage
-  Blogs
-  Projects
-  Reports
-end
