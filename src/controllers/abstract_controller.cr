b8ee66988049055686e29be2197584a19135ae76
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-06-05 21:25:47 +0200

    Refactor the `App::Controllers::AbstractController`

--- src/controllers/abstract_controller.cr
+++ src/controllers/abstract_controller.cr
@@ -1,22 +1,3 @@
 class App::Controllers::AbstractController < ATH::Controller
-  # Sends a JSON response easily.
-  protected def send_json(&block : IO -> _) : ATH::StreamedResponse
-    header = HTTP::Headers{"content-type" => "application/json; charset=UTF-8"}
-
-    ATH::StreamedResponse.new(headers: header) do |io|
-      block.call(io)
-    end
-  end
-
-  # Sends a JSON response with the status code and the message.
-  protected def send_json(status_code : Int32, message : String) : ATH::StreamedResponse
-    header = HTTP::Headers{"content-type" => "application/json; charset=UTF-8"}
-
-    ATH::StreamedResponse.new(headers: header, status: status_code) do |io|
-      io << {
-        status_code: status_code,
-        message:     message,
-      }
-    end
-  end
+  include App::Modules::ResponseSender
 end
