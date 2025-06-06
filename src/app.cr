fcaba4f9d46105e3c40c9c55026df957eda9830b
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-06-06 13:12:04 +0200

    Organize imports

--- src/app.cr
+++ src/app.cr
@@ -1,14 +1,16 @@
 require "./modules/**"
-require "./controllers/**"
+require "./resolvers/**"
 require "./database"
+require "./interfaces/**"
 require "./dto/**"
 require "./entities/**"
 require "./events/**"
 require "./helpers/**"
 require "./listeners/**"
 require "./repositories/**"
-require "./resolvers/**"
+require "./exceptions/**"
 require "./services/**"
+require "./controllers/**"
 
 module App
   VERSION = "0.1.0"
