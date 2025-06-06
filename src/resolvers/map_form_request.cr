8d85d5ead9c2108333e32b51671643260d63f33c
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-06-06 20:10:02 +0200

    Fix the `MapFormRequest` resolver.

--- src/resolvers/map_form_request.cr
+++ src/resolvers/map_form_request.cr
@@ -14,11 +14,9 @@ struct App::Resolvers::MapFormRequest
 
     return unless parameter.annotation_configurations.has? ::TZ::MapFormRequest
 
-    puts "Parameter type => #{parameter.type}"
-
     return blog_dto if parameter.type == App::DTO::BlogDTO
     return certification_dto if parameter.type == App::DTO::CertificationDTO
-    return project_dto if parameter.type == App::DTO::BlogDTO
+    return project_dto if parameter.type == App::DTO::ProjectDTO
     return report_dto if parameter.type == App::DTO::ReportDTO
   end
 end
