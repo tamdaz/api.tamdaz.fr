a32abde22574b9e04156b635dcdafbb8905e7b19
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-06-20 04:12:04 +0200

    Add helpers

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -6,6 +6,20 @@ require "athena/spec"
 require "./../src/commands/**"
 require "./../src/app"
 
+def send_form_data(&block : HTTP::FormData::Builder -> _) : String
+  io = IO::Memory.new
+
+  HTTP::FormData.build(io, "a4VF") do |builder|
+    block.call(builder)
+  end
+
+  io.to_s
+end
+
+def form_data_header : HTTP::Headers
+  HTTP::Headers{"Content-Type" => "multipart/form-data; boundary=\"a4VF\""}
+end
+
 Athena::Dotenv.new.overload ".env.test"
 
 ASPEC.run_all
